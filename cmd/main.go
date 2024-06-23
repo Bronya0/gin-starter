6af0e6cb2771cca44b0a22cb160dfbf2f913396b
bronya0 <tangssst@qq.com>
2024-06-24 00:49:29 +0800

    fix: conf

--- cmd/main.go
+++ cmd/main.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 	// 加载配置文件
-	utils.InitConfig(path.Join(global.RootPath, "config/config.yaml"))
+	utils.InitConfig(path.Join(global.RootPath, "conf/conf.yaml"))
 	// 初始化logger
 	logger.InitLogger()
 	// 连接数据库
