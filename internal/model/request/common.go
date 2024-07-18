4bc4f4917bccb901bfe1853c11156e6661be98b1
tangwuhong <tangwuhong@intra.nsfocus.com>
2024-07-18 15:08:22 +0800

    add：page util

--- internal/model/request/common.go
+++ internal/model/request/common.go
@@ -1,6 +1,24 @@
 package request
 
-type Page struct {
-	Page int `form:"page,default=1"`
-	Size int `form:"size,default=10"`
+import "errors"
+
+// Pagination Pagination.
+type Pagination struct {
+	PageSize int `form:"page_size" json:"page_size"`
+	PageNum  int `form:"page_num" json:"page_num"`
+}
+
+// Verify the value of pageNum and pageSize.
+func (p *Pagination) Verify() error {
+	if p.PageNum < 0 {
+		return errors.New("pageNum err")
+	} else if p.PageNum == 0 {
+		p.PageNum = 1
+	}
+	if p.PageSize < 0 {
+		return errors.New("pageSize err")
+	} else if p.PageSize == 0 {
+		p.PageSize = 10
+	}
+	return nil
 }
